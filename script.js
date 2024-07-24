ef80e19f4124fcfc5409bb9a72a5d34622928d75
Justa Guy <155912666+Caleb01a@users.noreply.github.com>
2024-07-24 06:00:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-let deferredPrompt;
-    window.addEventListener('beforeinstallprompt', (e) => {
-        deferredPrompt = e;
-    });
+// let deferredPrompt;
+//     window.addEventListener('beforeinstallprompt', (e) => {
+//         deferredPrompt = e;
+//     });
 
-window.addEventListener("DOMContentLoaded", (event) => {
-    const installApp = document.getElementById('installApp');
-    if (installApp) {
-    installApp.addEventListener('click', async () => {
-        if (deferredPrompt !== null) {
-            deferredPrompt.prompt();
-            const { outcome } = await deferredPrompt.userChoice;
-            if (outcome === 'accepted') {
-                deferredPrompt = null;
-            }
-        }
-    });
-    }
-});
+// window.addEventListener("DOMContentLoaded", (event) => {
+//     const installApp = document.getElementById('installApp');
+//     if (installApp) {
+//     installApp.addEventListener('click', async () => {
+//         if (deferredPrompt !== null) {
+//             deferredPrompt.prompt();
+//             const { outcome } = await deferredPrompt.userChoice;
+//             if (outcome === 'accepted') {
+//                 deferredPrompt = null;
+//             }
+//         }
+//     });
+//     }
+// });
 
+            installButton.addEventListener('click', () => {
 
+                deferredPrompt.prompt();
+
+                deferredPrompt.userChoice.then(choiceResult => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('App installed');
+                    } else {
+                        console.log('App installation declined');
+                    }
+
+                    installButton.style.display = 'none';
+                });
+            });
 
